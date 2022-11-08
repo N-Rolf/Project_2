ae296bb5f36331d600952c7c272b7aceaace225f
N-Rolf <neilrolf24@gmail.com>
2022-11-08 15:39:54 -0600

    more pinouts

--- ALU.sv
+++ ALU.sv
@@ -6,5 +6,4 @@ module ALU (
 );
 
 
-
 endmodule
--- ARM_processor.sv
+++ ARM_processor.sv
@@ -1,6 +1,8 @@
 //top level file for 10-bit processor
 module ARM_processor (
+    input logic CLKb, CLK, PKb,
     input logic [9:0] Data_in,
+	 output logic [9:0] LED_B,
     output logic [7:0] HEX0,
     output logic [7:0] HEX1,
     output logic [7:0] HEX2,
@@ -9,6 +11,7 @@ module ARM_processor (
     output logic [7:0] HEX5
 );
 
-
+	//clock key debounce
+	debouncer clean (.A_noisy(CLKb), .CLK50M(CLK), .A(CLK_clean)); //CLKb=KEY0, CLK=50mhz PIN_P11
 
 endmodule
