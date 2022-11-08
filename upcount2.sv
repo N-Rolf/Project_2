bfa24272cad28e77157c4d7b7176d376e9b7edb3
taitgowlovech <taitlyn.gowlovech@jacks.sdstate.edu>
2022-11-08 14:54:57 -0600

    Update upcount2.sv

--- upcount2.sv
+++ upcount2.sv
@@ -2,7 +2,13 @@ module upcount2 (
     input logic CLR, CLKb,
     output logic [1:0] CNT
 );
-
-
+	
+always_ff @ (negedge CLKb)
+begin
+	if (CLR)
+		CNT <= 1'b0;
+	else
+		CNT <= CNT + 1;
+	end
 
 endmodule
