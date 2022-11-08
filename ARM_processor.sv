c1fb2834e4eba65ed28046d7d16f15871231859a
N-Rolf <neilrolf24@gmail.com>
2022-11-08 14:42:42 -0600

    init init

--- ARM_processor.sv
+++ ARM_processor.sv
@@ -1,3 +1,4 @@
+//top level file for 10-bit processor
 module ARM_processor (
     input logic 
     output logic
---
+++ registerFile.sv
@@ -0,0 +1,10 @@
+module registerFile (
+    input logic [9:0] D,
+    input logic ENW, ENRO, ENR1, CLKb,
+    input logic [2:0] WRA, RDA0, RDA1,
+    output logic [9:0] Q0, Q1
+);
+
+
+
+endmodule
