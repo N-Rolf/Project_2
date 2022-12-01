921424bc13759e33d629e68aff6725a050f191dc
taitgowlovech <taitlyn.gowlovech@jacks.sdstate.edu>
2022-12-01 08:50:17 -0600

    decoder

--- 3to8decoder.sv
+++ 3to8decoder.sv
@@ -4,7 +4,26 @@ module 3to8decoder (
 	output logic [7:0] out
 	);
 	
-	always_comb
+always @( in or EN)
 	begin
-		if(EN)
-			
+
+      if (EN) 
+        begin
+          out=8'd0;
+          case (in)
+              3'b000: out[0]=1'b1;
+              3'b001: out[1]=1'b1;
+              3'b010: out[2]=1'b1;
+              3'b011: out[3]=1'b1;
+              3'b100: out[4]=1'b1;
+              3'b101: out[5]=1'b1;
+              3'b110: out[6]=1'b1;
+              3'b111: out[7]=1'b1;
+              default: out=8'd0;
+          endcase
+      end
+else 
+
+out=8'd0;
+end
+endmodule
--- registerFile.sv
+++ registerFile.sv
@@ -9,15 +9,14 @@ logic ENR1;
 assign ENR1 = 1'b1;
 
 
-
-
 logic [7:0] Rin, Rout0, Rout1;
 
 
+//Decode WRA, RDA0, and RDA1 3 to 8 bits
+3to8decoder write_decoder (.in(WRA), .EN(ENW), .out(Rin));
+3to8decoder read0_decoder (.in(RDA0), .EN(ENR0), .out(Rout0));
+3to8decoder read1_decoder (.in(RDA1), .EN(ENR1), .out(Rout1));
 
-
-//decode WRA and RAD0 and RDA1?
-DECODE
 
 trireg10 R0 (.D(D), .CLKb(CLKb), .Rin(Rin[0]), .Rout0(Rout0[0]), .Rout1(Rout1[0]), .Q0(Q0), .Q1(Q1));
 trireg10 R1 (.D(D), .CLKb(CLKb), .Rin(Rin[1]), .Rout0(Rout0[1]), .Rout1(Rout1[1]), .Q0(Q0), .Q1(Q1));
@@ -27,8 +26,6 @@ trireg10 R4 (.D(D), .CLKb(CLKb), .Rin(Rin[4]), .Rout0(Rout0[4]), .Rout1(Rout1[4]
 trireg10 R5 (.D(D), .CLKb(CLKb), .Rin(Rin[5]), .Rout0(Rout0[5]), .Rout1(Rout1[5]), .Q0(Q0), .Q1(Q1));
 trireg10 R6 (.D(D), .CLKb(CLKb), .Rin(Rin[6]), .Rout0(Rout0[6]), .Rout1(Rout1[6]), .Q0(Q0), .Q1(Q1));
 trireg10 R7 (.D(D), .CLKb(CLKb), .Rin(Rin[7]), .Rout0(Rout0[7]), .Rout1(Rout1[7]), .Q0(Q0), .Q1(Q1));
-
-
 
 
 endmodule
